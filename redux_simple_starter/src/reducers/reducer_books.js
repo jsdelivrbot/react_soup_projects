8b2ada7a88f72d56eebd447d15e5ef90b081cf47
Burak <burak.karatas@epias.com.tr>
2017-06-09 15:45:11 +0300

    redux

---
+++ redux_simple_starter/src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+/**
+ * Created by bkaratas on 6/9/17.
+ */
+
+export  default function () {
+    return[
+        {title: 'JavaScript'},
+        {title: 'Fi'},
+        {title: 'The Dark Tower'},
+        {title: 'Beste'}
+    ]
+}
