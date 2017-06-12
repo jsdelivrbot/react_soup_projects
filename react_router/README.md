6424487df03b27770a01798600146f380d2b1756
Burak <bur.karatas@gmail.com>
2017-06-12 16:30:09 +0300

    project structure s changed

--- react_router/README.md
+++ react_router/README.md
@@ -1,4 +1,3 @@
 ## ReduxSimpleStarter
 
 Interested in learning [Redux](https://www.udemy.com/react-redux/)?
-
---
+++ react_router/src/components/video_list.js
@@ -0,0 +1,25 @@
+/**
+ * Created by bkaratas on 08.06.2017.
+ */
+import React from 'react';
+import VideoListItem from './video_list_item'
+
+const VideoList = (props) => {
+
+    const videoItems = props.videos.map((video) => {
+        return (
+            <VideoListItem
+                onVideoSelect={props.onVideoSelect}
+                key={video.etag}
+                video={video}/>
+        );
+    });
+
+    return (
+        <ul className="col-md-4 list-group">
+            {videoItems}
+        </ul>
+    );
+};
+
+export default VideoList;
