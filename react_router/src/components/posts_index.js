ce02f8f024381524e6285ec5654f03f3813d88ab
Burak <bur.karatas@gmail.com>
2017-06-13 13:35:52 +0300

    router added

--- react_router/src/components/posts_index.js
+++ react_router/src/components/posts_index.js
@@ -1,20 +1,38 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions';
 
-class PostsIndex extends Component{
+class PostsIndex extends Component {
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.fetchPosts();
     }
 
-    render(){
+    render() {
         return (
             <div>
-                Posts Index
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
+
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    {post.title}
+                </li>
+            );
+        })
+    }
 }
 
-export default connect(null, {fetchPosts}) (PostsIndex);
+function mapStateToProps(state) {
+    return {posts: state.posts};
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
---
+++ react_router/src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class PostsNew extends Component {
+    render(){
+        return (
+            <div>
+                PostNew!
+            </div>
+        );
+    }
+}
+
+export default PostsNew;
