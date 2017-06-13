868dc9d0c6ffa2eb6c1fe73edeaf3eed63deb181
Burak <bur.karatas@gmail.com>
2017-06-13 16:13:52 +0300

    forms

--- react_router/src/components/posts_new.js
+++ react_router/src/components/posts_new.js
@@ -1,14 +1,32 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 
 class PostsNew extends Component {
-    render(){
+
+    render() {
         return (
-            <div>
-                PostNew!
+            <form>
+                <Field name="title" label="Title for Post" component={this.renderField}/>
+                <Field name="tags" label="Tags" component={this.renderField}/>
+                <Field name="content" label="Post Content" component={this.renderField}/>
+            </form>
+        );
+    }
+
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
             </div>
         );
     }
+
 }
 
-export default PostsNew;
+export default reduxForm({form: 'PostsNewForm'})(PostsNew);
